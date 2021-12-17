6c1ebde018070597a38e90446ed8c61880040d9a
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-12-17 17:06:05 +0530

    Created User Model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manages the user profiles"""
+    def create_user(self, email, fname, lname, dob, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, fname=fname, lname=lname, dob=dob)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, fname, lname, dob, password=None):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, fname, lname, dob, password)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database models for users (Making a completely different user model)"""
+    email = models.EmailField(max_length=255, unique=True)
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    dob = models.DateField()
+    otp = models.IntegerField(blank=True, null=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['fname', 'lname', 'dob']
+
+    def get_full_name(self):
+        return self.fname + " " + self.lname
+    
+    def get_email(self):
+        return self.email
+
+    def get_dob(self):
+        return self.dob
+    
+    def __str__(self):
+        return self.email
