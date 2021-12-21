70873b4a1167ad18558fc690ccdabb3203956eea
Jay Kumar Gaur <jaygaur99@gmail.com>
2021-12-21 21:46:20 +0530

    Added multiple address show on user profile

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/', views.profile, name='profile'),
+    path('profile/home', views.profile, name='profile'),
+    path('profile/address/', views.profile_address, name='address'),
     path('logout/', views.user_logout, name='logout'),
     path('login/', views.UserLogin.as_view(), name='login'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('forgot_password/', views.ForgotPassword.as_view(), name='forgot_password'),
     path('otp_check/', views.OTPCheck.as_view(), name='otp_check'),
     path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('profile/api/user/<int:user_id>/', views.update_phone, name='update_phone'),
 ]
