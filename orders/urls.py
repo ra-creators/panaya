e7e1b4ca5ea111bee2fd344258a78f3bc9162984
abdev <ab.inpathtoadev@gmail.com>
2021-12-22 19:58:25 +0530

    razor pay integration done

--- orders/urls.py
+++ orders/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.start, name="order_start"),
+    path('start/', views.start, name="order_start"),
+    path('order/<int:order_id>', views.order_details, name="order_details"),
     path('address/', views.address, name="order_address"),
     path('add_address/', views.add_address, name="order_add_address"),
-    path('order_details/', views.order, name="order_order_details"),
+    path('order_details/', views.verify_order, name="order_verify_details"),
     path('create_order/', views.create_order, name="order_create"),
-    path('rp_callback', views.rp_callback, name="rp_callback"),
 ]
---
+++ razor_pay/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import HttpResponse, redirect
+from django.views.decorators.csrf import csrf_exempt
+
+# models
+from .models import RazorPayOrder, Transaction
+from orders.models import Order
+
+# razor pay
+from .razorpay_key import razorpay
+
+
+@csrf_exempt
+def rp_callback(request):
+    if(request.method != 'POST'):
+        return HttpResponse('400')
+
+    data = request.POST
+    # 'razorpay_payment_id': ['pay_IaT9NuYNMv0KPu'],
+    # 'razorpay_order_id': ['order_IaT8u5a6oG3blH'],
+    # 'razorpay_signature': ['e199055fa86a73800b9d0605d1578255838438b110c691c55fd643830d046c1d'],
+    # 'org_logo': [''], # 'org_name': ['Razorpay Software Private Ltd'],
+    # 'checkout_logo': ['https://cdn.razorpay.com/logo.png'], 'custom_branding': ['false']
+    # print(data)
+    try:
+        res = razorpay.utility.verify_payment_signature(data)
+    except:
+        return HttpResponse('payment failure')
+
+    try:
+        order = Order.objects.get(razorpay_order_id=data['razorpay_order_id'])
+        rp_order = RazorPayOrder.objects.get(order=order)
+        Transaction.objects.create(
+            order=order,
+            razorpay_order=rp_order,
+            payment_id=data['razorpay_payment_id'],
+            payment_sig=data['razorpay_signature'],
+        )
+        order.paid = True
+        order.save()
+        return redirect('order_details', order.id)
+    except:
+        return HttpResponse('error occured')
