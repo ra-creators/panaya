3d35d01137ed54b01311bb57cca9595f79652cf5
abdev <ab.inpathtoadev@gmail.com>
2021-12-29 19:15:16 +0530

    mail : post successfull transaction

--- utils/mail/order_confirmation.py
+++ utils/mail/order_confirmation.py
@@ -1,6 +1,4 @@
-from django.template.loader import render_to_string, get_template
-from django.template import Context
-from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 from django.conf import settings
 # from mail import send_mail
 from django.core.mail import send_mail
