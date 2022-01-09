a434145b1572e9bc6592183a069860de8a2df0be
abdev47 <ab.inpathtoadev@gmail.com>
2022-01-09 15:06:47 +0000

    server config

--- pages_static/apps.py
+++ pages_static/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class PagesStaticConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'pages_static'
+
+    def ready(self):
+        call_command('collectstatic', verbosity=0, interactive=False)
